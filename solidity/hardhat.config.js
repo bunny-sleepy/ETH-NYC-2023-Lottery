14a98a894edacc9a76dc64f8a6e4e24ae7be4616
bunny-sleepy <wang142857@hotmail.com>
2023-09-24 07:54:22 -0400

    deploy on testnets

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox")
 
+const privKey = ""
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
@@ -9,7 +11,28 @@ module.exports = {
     },
     localhost: {
       url: "http://0.0.0.0:8545",
-    }
+    },
+    arbitrumTest: {
+      url: "https://goerli-rollup.arbitrum.io/rpc",
+      accounts: [privKey]
+    },
+    lineaTest: {
+      url: "https://rpc.goerli.linea.build",
+      accounts: [privKey]
+    },
+    gnosisTest: {
+      url: "https://rpc.chiadochain.net",
+      accounts: [privKey],
+      gasPrice: 8000000000
+    },
+    scrollTest: {
+      url: "https://scroll-testnet-public.unifra.io",
+      accounts: [privKey],
+    },
+    mantleTest: {
+      url: "https://rpc.testnet.mantle.xyz",
+      accounts: [privKey],
+    },
   },
   solidity: "0.8.13",
 }
